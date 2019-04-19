2329a2c41b07c2b807f1978442966ddb03d645a7
Daniel <da16ni19@gmail.com>
2019-04-19 18:01:11 -0500

    recent changes

--- offsiteTrackingFrontEnd/src/app/app.module.ts
+++ offsiteTrackingFrontEnd/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UserComponent } from './user/user.component';
+import { CustomMaterialModule } from './core/material.module';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
