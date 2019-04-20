fdd7e63107b680cd4e551faecf8a1a53e3c8822e
Ariunjin <ariunjin@gmail.com>
2019-04-19 19:01:11 -0500

    PlaceModel

---
+++ backend/models/PlaceModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    placeName: {
+        type: String,
+        required: true
+    },
+    address: {
+        state: { type: String, required: true },
+        city: { type: String, required: true },
+        street: { type: String, required: true },
+        state: { type: String, required: true },
+        location: [Number, Number],
+    },
+    schedule: [
+        {
+            employeeId: { type: Number, required: true },
+            date: {
+                type: Date, required: true,
+                default: Date.now
+            },
+            description: { type: String, required: true }
+        }
+    ]
+}, { collection: 'places' });
+
+module.exports = mongoose.model('User', userSchema);
