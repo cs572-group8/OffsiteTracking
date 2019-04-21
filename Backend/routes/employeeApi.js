35c5785d632db6a986b9014e2d682fffbc4c1ceb
Ariunjin <ariunjin@gmail.com>
2019-04-21 12:52:31 -0500

    EmployeeUpdate

--- Backend/routes/employeeApi.js
+++ Backend/routes/employeeApi.js
@@ -3,23 +3,14 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../models/UserModel');
 
-router.get('/save', (req, res) => {
-    let user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        jobTitle: req.body.jobTitle,
-        address: req.body.address,
-        city: req.body.city,
-        state: req.body.state,
-        phone: req.body.phone,
-        email: req.body.email, 
-        type: req.body.type         
-    });
-
-    user.save(function (err, firstName) {
-        if (err)
-            res.status(500).send("Failed");;
-        res.json(user);
+router.post('/save', (req, res) => {
+    const newUser = new User(req.body);
+    newUser.password = '123';
+    newUser.save((err) => {
+        if (err) throw err;
+        res.json({
+            success: "Inserted New employee to database"
+        })
     });
 });
 
